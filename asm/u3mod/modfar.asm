57e4269d6547de6b466cf16353fc85a070a6518f
mcmagi <devnull@localhost>
2017-01-17 08:14:30 -0600

    add LoLB mod

--- asm/u3mod/modfar.asm
+++ asm/u3mod/modfar.asm
@@ -17,82 +17,82 @@ GET_POI_INDEX_FAR:
     push ds
     call SET_SEGMENT
     call GET_POI_INDEX
-    push ds
+    pop ds
     retf
 
 GET_POI_STR_FAR:
     push ds
     call SET_SEGMENT
     call GET_POI_STR
-    push ds
+    pop ds
     retf
 
 GET_WORLD_STR_FAR:
     push ds
     call SET_SEGMENT
     call GET_WORLD_STR
-    push ds
+    pop ds
     retf
 
 GET_WHIRLPOOL_STR_FAR:
     push ds
     call SET_SEGMENT
     call GET_WHIRLPOOL_STR
-    push ds
+    pop ds
     retf
 
 GET_PARTY_STR_FAR:
     push ds
     call SET_SEGMENT
     call GET_PARTY_STR
-    push ds
+    pop ds
     retf
 
 GET_ROSTER_STR_FAR:
     push ds
     call SET_SEGMENT
     call GET_ROSTER_STR
-    push ds
+    pop ds
     retf
 
 GET_MOONGATE_COORDS_FAR:
     push ds
     call SET_SEGMENT
     call GET_MOONGATE_COORDS
-    push ds
+    pop ds
     retf
 
 GET_START_FAR:
     push ds
     call SET_SEGMENT
     call GET_START
-    push ds
+    pop ds
     retf
 
 GET_NEW_MOON_TOWN_OFFSET_FAR:
     push ds
     call SET_SEGMENT
     call GET_NEW_MOON_TOWN_OFFSET
-    push ds
+    pop ds
     retf
 
 GET_SNAKE_TELEPORT_FAR:
     push ds
     call SET_SEGMENT
     call GET_SNAKE_TELEPORT
-    push ds
+    pop ds
     retf
 
 IS_EXOTIC_WEAPON_FAR:
     push ds
     call SET_SEGMENT
     call IS_EXOTIC_WEAPON
-    push ds
+    pop ds
     retf
 
 IS_EXOTIC_ARMOR_FAR:
     push ds
     call SET_SEGMENT
     call IS_EXOTIC_ARMOR
-    push ds
+    pop ds
     retf
